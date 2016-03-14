67e49119ac0016b0126a4a9197862d199f7df15a
Russell Bradley <me@russellbradley.com>
2016-03-14 02:56:34 -0400

    figure out system os

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+platform="unknown"
+platformOs=`uname`
+if [[ "$platformOs" == "Linux" ]]; then
+	platform='linux'
+elif [[ "$platformOs" == "Darwin" ]]; then
+	platform='darwin'
+fi
+
 # install vundle which manages the vim plugins
 git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install oh-my-zsh
 # https://github.com/robbyrussell/oh-my-zsh
+if [[ "$platform" == "linux" ]]; then 
+	sudo apt-get install zsh
+fi 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 
 # setup RussellBradley/vimrc 
 # git@github.com:RussellBradley/vimrc.git
