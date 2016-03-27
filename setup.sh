8915385c334e2f99473303972b3c68fb1caa3cbe
Russell Bradley <me@russellbradley.com>
2016-03-27 00:24:19 -0400

    add python-setuptools

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ fi
 
 # install linux specific apps via apt-get - zsh, tmux, vim, htop
 if [[ "$platform" == "linux" ]]; then
-	sudo apt-get install zsh tmux vim htop upower
+	sudo apt-get install zsh tmux vim htop upower python-setuptools
 	if ! grep -q "zsh" ~/.bashrc; then
 		printf "# Enable zsh once ssh'd into box\nzsh\n" | cat - ~/.bashrc > temp && mv temp ~/.bashrc
 	fi
